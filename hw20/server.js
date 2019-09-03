ca7b27ddbe5de0b921c0a1fcb57043287b6c5a85
Viktor <viktor@dsi.io>
2019-09-03 17:14:19 +0300

    Homework for lesson 20 done

--- hw20/server.js
+++ hw20/server.js
@@ -1,4 +1,7 @@
 const http = require('http'); // подключаем http модуль!
 http.createServer((req, res) => {
-    res.end('HELLO WORLD');
+    if(req.url == '/data'){
+        res.end('success');
+    };
+    res.end(JSON.stringify({ name: 'SERGEi', lastName: 'SHAKHOV' }));
 }).listen(3000, '127.0.0.1', () => console.log('Server is listening on port:' + 3000));
